3d1a1eed6ba92c18e930bb7a1e97246477716ac5
Sumod Madhavan <sumadh@microsoft.com>
2020-12-15 13:46:51 +0530

    Cleanup

--- Sahaj.Wiki/Sahaj.Wiki/ParagraphMatching.cs
+++ Sahaj.Wiki/Sahaj.Wiki/ParagraphMatching.cs
@@ -1,10 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
 namespace Sahaj.Wiki
 {
     public class ParagraphMatching
     {
+        private readonly string[] wordstoIgnore = { "whose", "which", "why" };
         public ParagraphMatching()
         {
+        }
+        //brute force implementation. Need improvement on Algo
+        //ref : https://en.wikipedia.org/wiki/Hungarian_algorithm
+        //TODO: read and give assignments
+        //Time O(n2) Space O(n2)
+        public SortedList<int,string> AnalyzeText(string[] sentences, string[] questions, string[] answers)
+        {
+            SortedList<int, string> sortedList = new SortedList<int, string>() ;
+            List<string> answerList = new List<string>(answers);
+            int initalIndex = 1;
+            foreach (string eachQuestion in questions)
+            {
+                var totalWords = Regex.Matches(eachQuestion, "[\\w-]*\\w+|\".+\"")
+                                 .Cast<Match>()
+                                 .Select(i => i.Value.ToLower())
+                                 .Where(s => !wordstoIgnore.Any(i => i.Equals(s)));
+
+                int m = 0;
+                string answer = "";
+                for (int i = 0; i < sentences.Length; ++i)
+                {
+                    var sentence = sentences[i].ToLower();
+                    int score = 0;
+                    foreach (var word in totalWords)
+                    {
+                        if (sentence.IndexOf(word) >= 0)
+                        {
+                            score += word.Length;
+                        }
+                        // if plurals
+                        else if (sentence.IndexOf(word.TrimEnd('s')) >= 0)
+                        {
+                            score += word.Length;
+                        }
+                    }
+                    if (score > m)
+                    {
+                        foreach (var a in answerList)
+                        {
+                            if (sentence.Contains(a.ToLower()))
+                            {
+                                answer = a;
+                                m = score;
+                                break;
+                            }
+                        }
+                    }
+                }
+                sortedList.Add(initalIndex, answer.Trim());
+                Console.WriteLine(answer);
+                answerList.Remove(answer);
+                initalIndex++;
+            }
+            return sortedList;
         }
     }
 }
--- Sahaj.Wiki/Sahaj.Wiki/Program.cs
+++ Sahaj.Wiki/Sahaj.Wiki/Program.cs
@@ -1,72 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace Sahaj.Wiki
 {
     class Program
     {
-        static readonly string[] wordstoIgnore = { "whose", "which", "why" };
+        
         static void Main(string[] args)
         {
+            Console.WriteLine("Input Sentence");
             string[] sentences = Console.ReadLine().Split('.');
+            Console.WriteLine("Input 5 Questions");
             string[] questions = new string[5];
             for (int i = 0; i < 5; ++i)
                 questions[i] = Console.ReadLine();
-
+            Console.WriteLine("Input ; seperated Answers");
             string[] answers = Console.ReadLine().Split(';');
-
-            ParagraphMatching(sentences, questions, answers);
-        }
-        //brute force implementation.
-        //ref : https://en.wikipedia.org/wiki/Hungarian_algorithm
-        //TODO: read and give assignments
-        //Time O(n2) Space O(n2)
-        static void ParagraphMatching(string[] sentences, string[] questions, string[] answers)
-        {
-            List<string> ans = new List<string>(answers);
-            foreach (string eachQuestion in questions)
-            {
-                var totalWords = Regex.Matches(eachQuestion, "[\\w-]*\\w+|\".+\"")
-                                 .Cast<Match>()
-                                 .Select(i => i.Value.ToLower())
-                                 .Where(s => !wordstoIgnore.Any(i => i.Equals(s)));
-
-                int m = 0;
-                string answer = "";
-                for (int i = 0; i < sentences.Length; ++i)
-                {
-                    var sentence = sentences[i].ToLower();
-                    int score = 0;
-                    foreach (var word in totalWords)
-                    {
-                        if (sentence.IndexOf(word) >= 0)
-                        {
-                            score += word.Length;
-                        }
-                        // if plurals
-                        else if (sentence.IndexOf(word.TrimEnd('s')) >= 0)
-                        {
-                            score += word.Length;
-                        }
-                    }
-                    if (score > m)
-                    {
-                        foreach (var a in ans)
-                        {
-                            if (sentence.Contains(a.ToLower()))
-                            {
-                                answer = a;
-                                m = score;
-                                break;
-                            }
-                        }
-                    }
-                }
-                Console.WriteLine(answer);
-                ans.Remove(answer);
-            }
+            ParagraphMatching paragraphMatching = new ParagraphMatching();
+            _ = paragraphMatching.AnalyzeText(sentences, questions, answers);
         }
     }
 }
