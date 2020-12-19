ae00df4a64ad39aad0ed8ae3beb5edab32705005
Sumod Madhavan <sumadh@microsoft.com>
2020-12-19 09:19:29 +0530

    adding more tests

--- Sahaj.Wiki/Sahaj.Wiki.Test/ParagraphMatchTest.cs
+++ Sahaj.Wiki/Sahaj.Wiki.Test/ParagraphMatchTest.cs
@@ -40,5 +40,90 @@ namespace Sahaj.Wiki.Test
             Assert.AreEqual("the plains zebra, the Grévy's zebra and the mountain zebra", result["Which are the three species of zebras?"].Data);
             Assert.AreEqual("subgenus Hippotigris", result["Which subgenus do the plains zebra and the mountain zebra belong to?"].Data);
         }
+
+        [TestMethod]
+        [DataRow
+           ("Zebras are several species of African equids (horse family) united by their distinctive black and white stripes." +
+           " Their stripes come in different patterns, unique to each individual. " +
+           "They are generally social animals that live in small harems to large herds." +
+           " Unlike their closest relatives, horses and donkeys, zebras have never been truly domesticated." +
+           " There are three species of zebras: the plains zebra, the Grévy's zebra and the mountain zebra. " +
+           "The plains zebra and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus Dolichohippus. " +
+           "The latter resembles an ass, to which it is closely related, while the former two are more horse-like. All three belong to the genus Equus, along with other living equids." +
+           " The unique stripes of zebras make them one of the animals most familiar to people. " +
+           "They occur in a variety of habitats, such as grasslands, savannas, woodlands, thorny scrublands, mountains, and coastal hills. However, various anthropogenic factors have had a severe impact on zebra populations, in particular, hunting for skins and habitat destruction. " +
+           "Grévy's zebra and the mountain zebra are endangered. While plains zebras are much more plentiful, one subspecies - the Quagga - became extinct in the late 19th century. Though there is currently a plan, called the Quagga Project, that aims to breed zebras that are phenotypically similar to the Quagga, in a process called breeding back​.",
+           DisplayName = " should pass for sentence extraction")]
+        public void Test_if_Sentence_Extraction_Pass(string sentencesString)
+        {
+            ProcessWiki processWiki = new ProcessWiki
+            {
+                Sentences = sentencesString
+            };
+            bool result = processWiki.ExtractSentences();
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        [DataRow
+           ("Zebras are several species of African equids (horse family) united by their distinctive black and white stripes." +
+           " Their stripes come in different patterns, unique to each individual. " +
+           "They are generally social animals that live in small harems to large herds." +
+           " Unlike their closest relatives, horses and donkeys, zebras have never been truly domesticated." +
+           " There are three species of zebras: the plains zebra, the Grévy's zebra and the mountain zebra. " +
+           "The plains zebra and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus Dolichohippus. " +
+           "The latter resembles an ass, to which it is closely related, while the former two are more horse-like. All three belong to the genus Equus, along with other living equids." +
+           " The unique stripes of zebras make them one of the animals most familiar to people. " +
+           "They occur in a variety of habitats, such as grasslands, savannas, woodlands, thorny scrublands, mountains, and coastal hills. However, various anthropogenic factors have had a severe impact on zebra populations, in particular, hunting for skins and habitat destruction. " +
+           "Grévy's zebra and the mountain zebra are endangered. While plains zebras are much more plentiful, one subspecies - the Quagga - became extinct in the late 19th century. Though there is currently a plan, called the Quagga Project, that aims to breed zebras that are phenotypically similar to the Quagga, in a process called breeding back​.",
+           DisplayName = " Should fail for sentence extraction")]
+        public void Test_if_Sentence_Extraction_Fail(string sentencesString)
+        {
+            ProcessWiki processWiki = new ProcessWiki
+            {
+                Sentences = ""
+            };
+            bool result = processWiki.ExtractSentences();
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [DataRow
+           (
+           "Which Zebras are endangered?," +
+           "What is the aim of the Quagga Project?," +
+           "Which animals are some of their closest relatives?," +
+           "Which are the three species of zebras?," +
+           "Which subgenus do the plains zebra and the mountain zebra belong to?",
+           DisplayName = " Should pass with correct questions as input")]
+        public void Test_if_Question_Extraction_pass(string questionsString)
+        {
+            ProcessWiki processWiki = new ProcessWiki
+            {
+                Questions = questionsString,
+                
+            };
+            var result = processWiki.ExtractQuestions();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow
+           (
+           "Which Zebras are endangered?," +
+           "What is the aim of the Quagga Project?," +
+           "Which animals are some of their closest relatives?," +
+           "Which are the three species of zebras?," +
+           "Which subgenus do the plains zebra and the mountain zebra belong to?",
+           DisplayName = " Should fail questions is null")]
+        public void Test_if_Question_Extraction_Fail(string questionsString)
+        {
+            ProcessWiki processWiki = new ProcessWiki
+            {
+                Questions = null,
+
+            };
+            var result = processWiki.ExtractQuestions();
+            Assert.IsFalse(result);
+        }
     }
 }
